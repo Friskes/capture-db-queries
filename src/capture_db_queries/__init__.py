4ff3d4ddd4b9b3ee9478ac2a4b6997241ace673e
Friskes <friskestv@gmail.com>
2024-11-27 16:53:36 +0300

    ignore cgi DeprecationWarning

--- src/capture_db_queries/__init__.py
+++ src/capture_db_queries/__init__.py
@@ -1,2 +1,10 @@
-from ._logging import switch_logger, switch_trace  # noqa: F401
-from .decorators import CaptureQueries, ExtCaptureQueriesContext, capture_queries  # noqa: F401
+import warnings
+
+warnings.filterwarnings(
+    'ignore',
+    message="'cgi' is deprecated and slated for removal in Python 3.13",
+    category=DeprecationWarning,
+)
+
+from ._logging import switch_logger, switch_trace  # noqa: F401, E402
+from .decorators import CaptureQueries, ExtCaptureQueriesContext, capture_queries  # noqa: F401, E402
